c2762317a2f80824fc9bd40e3cbc5ef09b599e6e
nathanisfun200 <nathanisfun200@gmail.com>
2023-03-09 20:03:42 -0600

    add styling, clear, fixed decimal points and output bug fixes

--- calc.js
+++ calc.js
@@ -12,7 +12,11 @@ const calculation = function() {
             a.textContent = (Number(b[0]) * Number(b[2]));
             break;
         case '÷':
-            a.textContent = (Number(b[0]) / Number(b[2]));
+            if ((Number(b[0]) / Number(b[2])) % 1 === 0) {
+                a.textContent = (Number(b[0]) / Number(b[2]));
+            } else {
+                a.textContent = (Number(b[0]) / Number(b[2])).toFixed(3);
+            };
             break;
         default:
             a.textContent += '';
@@ -40,18 +44,30 @@ operators.forEach(element => {
 
 
 
-let fontFactor = 3;
+let fontFactor = 2;
 function textSize() {
+
     let output = document.querySelector('#display').offsetWidth;
     let outputText = document.querySelector(".output-container");
-    console.log(output, outputText)
     if (output >= 380) {
-        outputText.style.fontSize = `${60 - fontFactor}px`
+        outputText.style.fontSize = `${60 - fontFactor}px`;
         fontFactor += 3;
+    }
+
+    let a = document.querySelector('#display');
+    if (a.textContent.split('').length < 11) {
+        outputText.style.fontSize = '60px';
+        fontFactor = 2;
     }
 }
 
 let button = document.querySelectorAll('button');
 button.forEach(button => {
     button.addEventListener('click', textSize)
+});
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', function() {
+    const text = document.querySelector('#display')
+    text.textContent = '';
 });
